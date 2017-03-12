334ab66e5d909df39a9458614114000ba5747f32
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-03-12 17:10:58 +0900

    init

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,46 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
+
+from .models import Question, Choice
 
 
-def index(request):
-    '''index'''
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(generic.ListView):
+    context_object_name = 'latest_question_list'
 
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/question_results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        redirect = reverse('polls:results', args=(question.id,))
+        return HttpResponseRedirect(redirect)
+
+    return HttpResponse("You're voting on question %s." % question_id)
